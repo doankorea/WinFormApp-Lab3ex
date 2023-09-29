f17a2b3581ab7870e6ec1988213f876776697db6
Doankorea <tddnhny@gmail.com>
2023-09-29 07:34:17 +0700

    lab3ex

---
+++ lab_3ex/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace lab_3ex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbbODia = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbbThuMuc = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLoiBaiHat = new System.Windows.Forms.RichTextBox();
+            this.lbDanhSach = new System.Windows.Forms.ListBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ổ đĩa";
+            // 
+            // cbbODia
+            // 
+            this.cbbODia.FormattingEnabled = true;
+            this.cbbODia.Location = new System.Drawing.Point(86, 24);
+            this.cbbODia.Name = "cbbODia";
+            this.cbbODia.Size = new System.Drawing.Size(121, 21);
+            this.cbbODia.TabIndex = 1;
+            this.cbbODia.SelectedIndexChanged += new System.EventHandler(this.cbbODia_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Thư mục";
+            // 
+            // cbbThuMuc
+            // 
+            this.cbbThuMuc.FormattingEnabled = true;
+            this.cbbThuMuc.Location = new System.Drawing.Point(86, 71);
+            this.cbbThuMuc.Name = "cbbThuMuc";
+            this.cbbThuMuc.Size = new System.Drawing.Size(121, 21);
+            this.cbbThuMuc.TabIndex = 3;
+            this.cbbThuMuc.SelectedIndexChanged += new System.EventHandler(this.cbbThuMuc_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tập tin";
+            // 
+            // txtLoiBaiHat
+            // 
+            this.txtLoiBaiHat.Location = new System.Drawing.Point(384, 24);
+            this.txtLoiBaiHat.Name = "txtLoiBaiHat";
+            this.txtLoiBaiHat.Size = new System.Drawing.Size(404, 414);
+            this.txtLoiBaiHat.TabIndex = 5;
+            this.txtLoiBaiHat.Text = "";
+            // 
+            // lbDanhSach
+            // 
+            this.lbDanhSach.FormattingEnabled = true;
+            this.lbDanhSach.Location = new System.Drawing.Point(38, 156);
+            this.lbDanhSach.Name = "lbDanhSach";
+            this.lbDanhSach.Size = new System.Drawing.Size(267, 134);
+            this.lbDanhSach.TabIndex = 7;
+            this.lbDanhSach.SelectedIndexChanged += new System.EventHandler(this.lbDanhSach_SelectedIndexChanged);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(38, 306);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(267, 132);
+            this.axWindowsMediaPlayer1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbDanhSach);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.txtLoiBaiHat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbbThuMuc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbbODia);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbbODia;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbbThuMuc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox txtLoiBaiHat;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListBox lbDanhSach;
+    }
+}
+
---
+++ lab_3ex/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_3ex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AddCC();
+            
+            
+        }
+        private void AddCC()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo d in drives)
+            {
+                cbbODia.Items.Add(d.Name);
+            }
+        }
+        private void cbbODia_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DirectoryInfo Directory = new DirectoryInfo("D:\\");
+            DirectoryInfo[] Directories = Directory.GetDirectories("*.*");
+            FileInfo[] files = Directory.GetFiles();
+            foreach(DirectoryInfo d in Directories)
+            {
+                cbbThuMuc.Items.Add(d.Name);
+            }
+        }
+        private void cbbThuMuc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            lbDanhSach.Items.Clear();
+            txtLoiBaiHat.Text = string.Empty;
+            string[] files = Directory.GetFiles(cbbODia.Text + cbbThuMuc.Text);
+            foreach (string d in files)
+            {
+                lbDanhSach.Items.Add(d);
+            }
+        }
+        private void lbDanhSach_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbDanhSach.SelectedIndex != -1)
+            {
+                string selectedSongPath= lbDanhSach.SelectedIndex.ToString();
+
+                string lyricsFilePath=    Path.ChangeExtension(selectedSongPath, ".txt");
+                if(File.Exists(lyricsFilePath))
+                {
+                    string lyrics= File.ReadAllText(lyricsFilePath);
+                    txtLoiBaiHat.Text= lyrics;
+                }
+                else
+                {
+                    txtLoiBaiHat.Text = "Khong co loi bai hat nay";
+                }
+            }
+        }
+
+    }
+}
